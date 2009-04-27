bd95cd6850fe6425c35f66eeb79269af77b4d333
Erik Colson <eco@ecocode.net>
2009-04-27 07:54:20 +0200

    inc dir added in releases

--- lib/Finance/QuoteDB.pm
+++ lib/Finance/QuoteDB.pm
@@ -19,7 +19,7 @@ Finance::QuoteDB - User database tools based on Finance::Quote
 @EXPORT = ();
 @EXPORT_OK = qw /createdb updatedb addstock/ ;
 @EXPORT_TAGS = ( all => [@EXPORT_OK] );
-$VERSION = '0.07';
+$VERSION = '0.08';
 
 =head1 SYNOPSIS
 
