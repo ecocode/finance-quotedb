3ea774d81d7d8771dd7024a9a0b7a6c5836a454b
Erik Colson <eco@ecocode.net>
2009-05-03 18:46:25 +0200

    Some required default values set

--- lib/Finance/QuoteDB/Geniustrader.pm
+++ lib/Finance/QuoteDB/Geniustrader.pm
@@ -54,6 +54,7 @@ sub writeConfig {
     if ($dsn=~/;port=(\w+)(;.*)?$/) {
       $dbport = $1 ;
     }
+		print $fh "# Database specific stuff\n";
     print $fh "DB::genericdbi::db $db\n" if $db ;
     print $fh "DB::genericdbi::dbname $dbname\n" if $dbname ;
     print $fh "DB::genericdbi::dbhost $dbhost\n" if $dbhost ;
@@ -63,7 +64,16 @@ sub writeConfig {
     print $fh "DB::genericdbi::prices_sql SELECT day_open, day_high, day_low, day_close, volume, date ".
 			"FROM quote WHERE symbolID = '\$code' ORDER BY date DESC\n" ;
 		print $fh "DB::genericdbi::name_sql SELECT name FROM symbol WHERE symbolID = '\$code'\n" ;
+		print $fh "\n";
+		print $fh "# Stuff needed by geniustrader which you should set correctly\n";
+		print $fh "Graphics::Driver GD\n";
+		print $fh "Analysis::ReferenceTimeFrame year\n";
+		print $fh "Brokers::module SelfTrade\n";
+		print $fh "Path::Font::Arial ~/.gt/Arial.ttf\n";
+		print $fh "Path::Font::Courier ~/.gt/Geneva.ttf\n";
+		print $fh "Path::Font::Times ~/.gt/Times.ttf\n";
     close $fh ;
+		print "Configuration file $file written.\n";
   } else {
     ERROR ("Could not open $file in write mode") ;
   }
